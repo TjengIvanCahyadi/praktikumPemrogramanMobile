c5b4e2c5e95581a14ed3998669a6f62b98772dc4
Tjeng, Ivan Cahyadi <ivancahyadi77@gmail.com>
2022-04-17 22:24:42 +0800

    POSTTEST3 PM 2009106146

---
+++ posttest/posttest3_2009106146_tjengivancahyadi/lib/data_pengiriman.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'rincian_pesanan.dart';
+
+class DataPengiriman extends StatefulWidget {
+  String namaBarang, fotoBarang;
+  int hargaBarang, jumlahBarang;
+
+  DataPengiriman(
+      this.namaBarang, this.hargaBarang, this.jumlahBarang, this.fotoBarang);
+
+  @override
+  State<DataPengiriman> createState() =>
+      _DataPengirimanState(namaBarang, hargaBarang, jumlahBarang, fotoBarang);
+}
+
+class _DataPengirimanState extends State<DataPengiriman> {
+  String namaBarang, fotoBarang;
+  int hargaBarang, jumlahBarang;
+
+  _DataPengirimanState(
+      this.namaBarang, this.hargaBarang, this.jumlahBarang, this.fotoBarang);
+
+  var namaLengkap = "";
+  var nomorTelepon = "";
+  var alamat = "";
+
+  final namaLengkapCTRL = new TextEditingController();
+  final nomorTeleponCTRL = new TextEditingController();
+  final alamatCTRL = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: namaLengkapCTRL,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Nama Lengkap",
+                  hintText: "Isi Nama Lengkap",
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                controller: nomorTeleponCTRL,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Nomor Telepon",
+                  hintText: "Isi Nomor Telepon",
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                controller: alamatCTRL,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Alamat",
+                  hintText: "Isi Alamat",
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      namaLengkap = namaLengkapCTRL.text;
+                      nomorTelepon = nomorTeleponCTRL.text;
+                      alamat = alamatCTRL.text;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RincianPesanan(
+                            namaBarang,
+                            fotoBarang,
+                            hargaBarang,
+                            jumlahBarang,
+                            namaLengkap,
+                            nomorTelepon,
+                            alamat,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Text("Checkout"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
